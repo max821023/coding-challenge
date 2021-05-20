1ebcb93961661bce9ed83c45030b4f50570c26b7
max821023 <max821023@gmail.com>
2021-05-19 20:22:15 -0700

    Add create pizza route

--- controllers/pizza.ts
+++ controllers/pizza.ts
@@ -1,5 +1,29 @@
 import { NextFunction, Request, Response } from 'express';
-import Pizza from '../models/pizza';
+import mongoose from 'mongoose';
+import Pizza from '../models/Pizza';
+
+const createPizza = (req: Request, res: Response, next: NextFunction) => {
+  let { name, ingredients } = req.body;
+
+  const pizza = new Pizza({
+    _id: new mongoose.Types.ObjectId(),
+    name,
+    ingredients
+  });
+
+  return pizza.save()
+    .then(result => {
+      return res.status(201).json({
+        pizza: result
+      });
+    })
+    .catch(error => {
+      return res.status(500).json({
+      message: error.message,
+      error
+      });
+    })
+};
 
 const getAllPizzas = (req: Request, res: Response, next: NextFunction) => {
   Pizza.find()
@@ -17,4 +41,4 @@ const getAllPizzas = (req: Request, res: Response, next: NextFunction) => {
   });
 };
 
-export default { getAllPizzas };
+export default { createPizza, getAllPizzas };
--- routes/pizza.ts
+++ routes/pizza.ts
@@ -3,6 +3,7 @@ import controller from '../controllers/pizza';
 
 const router = express.Router();
 
+router.post('/create/pizza', controller.createPizza);
 router.get('/get/pizzas', controller.getAllPizzas);
 
 export = router;
